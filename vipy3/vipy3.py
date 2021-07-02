f36b351918e0b121e5b12e07578afe1ebc7bed92
TutajITeraz <boldalukasz@gmail.com>
2021-07-02 15:54:52 +0200

    functions for Node

--- vipy3/vipy3.py
+++ vipy3/vipy3.py
@@ -4,13 +4,42 @@ from helpers import *
 class Node:
     def __init__(self, meta_node=None, serialized_state=None):
         self.uuid = gen_uuid()
+        self.name = self.get_class_name()
         self.meta_node=meta_node
 
-        if serialized_state:
-            self.deserialize()
+        self.inputs = {}
+        self.outputs = {}
+        self.stage = 0
 
-    def getUuid(self):
+        self.fresh = False
+
+        if serialized_state:
+            self.deserialize(serialized_state)
+        else:
+            self.initialize()
+
+    def initialize(self):
+        pass
+
+    def get_name(self):
+        return self.name
+
+    def get_uuid(self):
         return self.uuid
+
+    def set_fresh(self,is_fresh):
+        self.fresh = is_fresh
+
+    def get_fresh(self):
+        fresh = self.fresh
+        #TODO - check if params are fresh too
+        return fresh
+
+    def set_stage(self, stage):
+        self.stage = stage
+        LOG.log(self.name + "(" + self.className + ")\t" + str(
+            self.execCounter) + "\t changed stage to: " + self.getStageName(stage))
+
 
     def serialize(self):
         pass
@@ -28,24 +57,23 @@ class MetaNode(Node):
     def __init__(self, workspace, uuid='',serialized_state=''):
         super().__init__()
         self.parent_workspace = workspace
-        self.name = self.get_class_name()
 
         if uuid != '':
             self.uuid = uuid
 
-        self.dpg_render_editor()
-
         if serialized_state != '':
             self.deserialize(serialized_state)
+        else:
+            self.initialize()
 
-    def get_name():
-        return self.name
+    def initialize(self):
+        self.dpg_render_editor()
 
     def render_node(self):
         pass
 
     def dpg_render_editor(self):
-        self.dpg_window_id = dpg.add_window(label=self.uuid, width=800, height=600, pos=(50, 50))
+        self.dpg_window_id = dpg.add_window(label=self.get_name(), width=800, height=600, pos=(50, 50))
 
         self.dpg_menu_bar_id = dpg.add_menu_bar(label='Workspace menu bar', parent=self.dpg_window_id)
         self.dpg_meta_node_menu_id = dpg.add_menu(label='MetaNode', parent=self.dpg_menu_bar_id)
@@ -54,7 +82,6 @@ class MetaNode(Node):
         dpg.add_menu_item(label='Save MetaNode As...', parent=self.dpg_meta_node_menu_id)#TODO save as meta_node
 
         self.dpg_add_node_menu_id = dpg.add_menu(label='Add Node...', parent=self.dpg_menu_bar_id)
-
         self.parent_workspace.dpg_render_available_nodes_to(self.dpg_add_node_menu_id)
 
         self.dpg_node_editor_id = dpg.add_node_editor(parent=self.dpg_window_id)
@@ -75,16 +102,21 @@ class MetaNode(Node):
 
     def serialize(self):
         status = {}
-        status['dpg_window_width'] = dpg.get_item_width(self.dpg_window_id)
-        status['dpg_window_height'] = dpg.get_item_height(self.dpg_window_id)
-        #status['dpg_window_pos'] = dpg.get_item_pos(self.dpg_window_id)
+        status['dpg_is_rendered'] = self.dpg_is_rendered
+        if self.dpg_is_rendered:
+            status['dpg_window_width'] = dpg.get_item_width(self.dpg_window_id)
+            status['dpg_window_height'] = dpg.get_item_height(self.dpg_window_id)
+            status['dpg_window_pos'] = dpg.get_item_pos(self.dpg_window_id)
         
         return status
 
     def deserialize(self, status):
-        dpg.set_item_width(self.dpg_window_id,status['dpg_window_width'])
-        dpg.set_item_height(self.dpg_window_id,status['dpg_window_height'])
-
+        self.dpg_is_rendered = status['dpg_is_rendered']
+        if self.dpg_is_rendered:
+            self.dpg_render_editor()
+            dpg.set_item_width(self.dpg_window_id,status['dpg_window_width'])
+            dpg.set_item_height(self.dpg_window_id,status['dpg_window_height'])
+            dpg.set_item_pos(self.dpg_window_id,status['dpg_window_pos'])
 
 
 
@@ -105,7 +137,7 @@ class Workspace:
         else:
             node = MetaNode(self)
 
-        meta_node_uuid = node.getUuid()
+        meta_node_uuid = node.get_uuid()
 
         self.meta_nodes[meta_node_uuid]=node
 
@@ -167,6 +199,7 @@ class Workspace:
         return status
 
     def deserialize(self,status):
+        #TODO should delete all existing meta nodes first
         LOG.log(status)
         self.nodes_available = status['nodes_available']
         meta_nodes_status = status['meta_nodes_status']
@@ -184,7 +217,7 @@ dpg_menu_bar_id = dpg.add_menu_bar(label='Workspace menu bar', parent=DPG_PRIMAR
 dpg_workspace_menu_id = dpg.add_menu(label='Workspace', parent=dpg_menu_bar_id)
 
 dpg.add_menu_item(label='New Workspace', parent=dpg_workspace_menu_id)#TODO new workspace
-dpg.add_menu_item(label='Load Last Workspace', parent=dpg_workspace_menu_id,callback=WORKSPACE.dpg_load_status_from_file_callback)#TODO load workspace
+dpg.add_menu_item(label='Load Last Workspace', parent=dpg_workspace_menu_id,callback=WORKSPACE.dpg_load_status_from_file_callback)
 dpg.add_menu_item(label='Load Workspace...', parent=dpg_workspace_menu_id)#TODO load workspace
 dpg.add_menu_item(label='Save Workspace', parent=dpg_workspace_menu_id, callback=WORKSPACE.dpg_save_status_to_file_callback)
 dpg.add_menu_item(label='Save Workspace As...', parent=dpg_workspace_menu_id)#TODO save as workspace
@@ -192,6 +225,10 @@ dpg.add_menu_item(label='New MetaNode', parent=dpg_workspace_menu_id, callback=W
 dpg.add_menu_item(label='Load MetaNode', parent=dpg_workspace_menu_id)#TODO load MetaNode
 
 dpg.set_primary_window(DPG_PRIMARY_WINDOW_ID, True)
+
+
+#Default to speed up the work:
+WORKSPACE.load_status_from_file()
 
 dpg.start_dearpygui()
 
