2ea9c12c89e29c916cdcdf8e812db8da096621df
TutajITeraz <41327134+TutajITeraz@users.noreply.github.com>
2021-07-15 22:02:43 +0200

    Update demo.py

--- vipy3/demo.py
+++ vipy3/demo.py
@@ -2,9 +2,3 @@ import dearpygui.dearpygui as dpg
 from dearpygui.demo import show_demo
 show_demo()
 dpg.start_dearpygui()
-
-with dpg.window(label="vipy3", width=800, height=800, pos=(100, 100), ) as demo_id:
-    with dpg.menu_bar():
-        with dpg.menu(label="File"):
-            dpg.add_menu_item(label="New")
-            dpg.add_menu_item(label="Open")
